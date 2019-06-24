efb4c2de11fa4a62bc36756537433f4fe136255f
Andrei <Knclav@gmail.com>
2019-06-24 20:14:43 +0200

    Added slider js and fix adaptive

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $('.basket-bonus__basket').on('mousemove', function(){
         $('.basket-bonus__basket-show').show();
     });
+    
     $('.basket-bonus__basket-show').on('mousemove', function(){
         $('.basket-bonus__basket-show').show();
     });
@@ -34,5 +35,42 @@ $(document).ready(function(){
     $('.basket-bonus__bonus').on('mouseleave', function(){
         $('.basket-bonus__bonus-show').hide();
     });
+
+    // added slider
+    
+    $('.slider__wrap').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        dotsClass: 'slider-dots',
+        adaptiveHeight: true,
+        mobileFirst: true,
+    });
+
+
+    
+    $(window).resize(function () {
+        let widthWind = $(this).width();
+    
+        // swich slider img
+
+        if (widthWind <= 768) {
+           $('.slider__img1').attr("src", "./img/slider/slider_bg2.jpg");
+           $('.slider__img2').attr("src", "./img/slider/slider_bg4.jpg");
+        }
+
+        else{
+            $('.slider__img1').attr("src", "./img/slider/slider_bg1.jpg");
+            $('.slider__img2').attr("src", "./img/slider/slider_bg3.jpg");
+        }
+
+        if (widthWind <= 992) {
+            $('.basket-bonus__basket').on('mousemove', function(){
+                $('.basket-bonus__basket-show').hide();
+            });
+         }
+    });                
+      
 
 });
